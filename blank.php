40a4e2e21df8185ddcb1d0df1e47da64db1142ae
Sai Deepak <sai@livewd.ca>
2026-04-15 16:18:23 -0700

    b

--- blank.php
+++ blank.php
@@ -122,46 +122,7 @@
                         <p>Paragraph</p>
                     </div><!-- End: main content -->
                     <!-- Start: Footer Centered -->
-                    <footer class="text-center py-5"><a
-                            class="text-decoration-none link-body-emphasis d-inline-flex align-items-center"
-                            href="#"><span class="fs-4 fw-bold brand-primary">ZENTRA</span><span
-                                class="fs-4 brand-secondary">CMS</span></a>
-                        <div class="d-flex justify-content-center align-items-center flex-wrap mb-2"><a
-                                class="link-body-emphasis mx-2" href="#">Privacy Policy</a><a
-                                class="link-body-emphasis mx-2" href="#">Terms of Service</a><a
-                                class="link-body-emphasis mx-2" href="#">Cookie Policy</a></div>
-                        <p class="text-muted mb-2">© 2026 Brand. All rights reserved.</p>
-                        <div class="fs-4 d-flex justify-content-center align-items-center gap-2 mb-2"><a
-                                class="link-body-emphasis" href="#"><svg
-                                    class="icon icon-tabler icon-tabler-brand-instagram text-muted"
-                                    xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24"
-                                    stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round"
-                                    stroke-linejoin="round">
-                                    <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
-                                    <path
-                                        d="M4 4m0 4a4 4 0 0 1 4 -4h8a4 4 0 0 1 4 4v8a4 4 0 0 1 -4 4h-8a4 4 0 0 1 -4 -4z">
-                                    </path>
-                                    <path d="M12 12m-3 0a3 3 0 1 0 6 0a3 3 0 1 0 -6 0"></path>
-                                    <path d="M16.5 7.5l0 .01"></path>
-                                </svg></a><a class="link-body-emphasis" href="#"><svg
-                                    class="icon icon-tabler icon-tabler-brand-x text-muted"
-                                    xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24"
-                                    stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round"
-                                    stroke-linejoin="round">
-                                    <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
-                                    <path d="M4 4l11.733 16h4.267l-11.733 -16z"></path>
-                                    <path d="M4 20l6.768 -6.768m2.46 -2.46l6.772 -6.772"></path>
-                                </svg></a><a class="link-body-emphasis" href="#"><svg
-                                    class="icon icon-tabler icon-tabler-brand-tiktok text-muted"
-                                    xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24"
-                                    stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round"
-                                    stroke-linejoin="round">
-                                    <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
-                                    <path
-                                        d="M21 7.917v4.034a9.948 9.948 0 0 1 -5 -1.951v4.5a6.5 6.5 0 1 1 -8 -6.326v4.326a2.5 2.5 0 1 0 4 2v-11.5h4.083a6.005 6.005 0 0 0 4.917 4.917z">
-                                    </path>
-                                </svg></a></div>
-                    </footer><!-- End: Footer Centered -->
+                    <?php include('_include/inner-footer.php'); ?>
                 </main>
             </div>
         </div>
