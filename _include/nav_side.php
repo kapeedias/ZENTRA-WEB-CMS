ae23e6b087c463ff2a7063006700ecdc922b2f05
Sai Deepak <sai@livewd.ca>
2026-04-15 15:32:36 -0700

    ui nav update

--- _include/nav_side.php
+++ _include/nav_side.php
@@ -39,13 +39,7 @@
                                     data-bs-toggle="collapse" role="button" aria-expanded="false"
                                     aria-controls="collapse-1" href="#collapse-1"><span> <i
                                             class="icon ion-ios-cog-outline me-2"></i>Admin
-                                        Settings</span><i class="bi bi-chevron-double-right"></i><svg
-                                        class="bi bi-chevron-right arrow" xmlns="http://www.w3.org/2000/svg" width="1em"
-                                        height="1em" fill="currentColor" viewBox="0 0 16 16">
-                                        <path fill-rule="evenodd"
-                                            d="M4.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L10.293 8 4.646 2.354a.5.5 0 0 1 0-.708">
-                                        </path>
-                                    </svg></a>
+                                        Settings</span><i class="icon ion-chevron-right arrow"></i></a>
                                 <div class="collapse" id="collapse-1">
                                     <ul class="nav ms-3">
                                         <li class="nav-item w-100"><a class="nav-link" href="#">Recent Orders</a></li>
