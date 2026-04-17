64db15aff32550d012c4444bb16345a39577c7fe
Sai Deepak <sai@livewd.ca>
2026-04-16 18:00:12 -0700

    test deb

--- myaccount.php
+++ myaccount.php
@@ -3,14 +3,14 @@
     ini_set('display_errors', 1);
     ini_set('display_startup_errors', 1);
     error_reporting(E_ALL);
-
+    require_once __DIR__ . '/config/helpers.php';
     secureSessionStart();
 
     // ==== CONFIG & DEPENDENCIES ====
     require_once __DIR__ . '/config/config.php';
     require_once __DIR__ . '/config/init.php';
     require_once __DIR__ . '/config/db.php';
-    require_once __DIR__ . '/config/helpers.php';
+
     require_once __DIR__ . '/classes/User.php';
 
     enforceSessionSecurity();
