8048708c06b02e79b226055c837c7040b7c6329b
Sai Deepak <sai@livewd.ca>
2026-04-16 18:01:23 -0700

    red

--- system-settings.php
+++ system-settings.php
@@ -1,11 +1,13 @@
 <?php
+
+    require_once __DIR__ . '/config/helpers.php';
     secureSessionStart();
 
     // ==== CONFIG & DEPENDENCIES ====
     require_once __DIR__ . '/config/config.php';
     require_once __DIR__ . '/config/init.php';
     require_once __DIR__ . '/config/db.php';
-    require_once __DIR__ . '/config/helpers.php';
+
     require_once __DIR__ . '/classes/User.php';
 
     enforceSessionSecurity();
