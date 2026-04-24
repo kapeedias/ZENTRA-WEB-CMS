01abdf72737775b85f33b7b23f2dc91f0ececfb4
Sai Deepak <sai@livewd.ca>
2026-04-23 17:26:51 -0700

    macc

--- myaccount.php
+++ myaccount.php
@@ -38,69 +38,7 @@
                     <?php include '_include/nav_top_branding.php'; ?>
                 </div>
                 <main class="px-3 px-md-4">
-                    <div
-                        class="d-flex flex-column justify-content-between flex-xl-row-reverse align-items-xl-start pt-3 mb-3 border-bottom">
-                        <div class="d-flex align-items-center mb-3 mb-xl-0">
-                            <form class="position-relative flex-grow-1 me-1"><input class="form-control pe-4"
-                                    type="search" placeholder="Search" name="search"><button
-                                    class="btn border-0 position-absolute top-50 end-0 translate-middle-y"
-                                    type="submit"><svg class="bi bi-search" xmlns="http://www.w3.org/2000/svg"
-                                        width="1em" height="1em" fill="currentColor" viewBox="0 0 16 16">
-                                        <path
-                                            d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001q.044.06.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1 1 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0">
-                                        </path>
-                                    </svg></button></form>
-
-                            <div class="dropdown"><button class="btn dropdown-toggle border-0 p-2"
-                                    data-bs-toggle="dropdown" aria-expanded="false" type="button"><img
-                                        class="object-fit-cover border rounded-circle"
-                                        src="assets/img/team/avatar2.jpg?h=7086b181e9fb853914a2cca97301c640" width="32"
-                                        height="32"></button>
-                                <div class="dropdown-menu dropdown-menu-end shadow"><a class="dropdown-item"
-                                        href="#"><svg class="bi bi-person me-2" xmlns="http://www.w3.org/2000/svg"
-                                            width="1em" height="1em" fill="currentColor" viewBox="0 0 16 16">
-                                            <path
-                                                d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6m2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0m4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4m-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10s-3.516.68-4.168 1.332c-.678.678-.83 1.418-.832 1.664z">
-                                            </path>
-                                        </svg>&nbsp;Profile</a><a class="dropdown-item" href="#"><svg
-                                            class="bi bi-gear me-2" xmlns="http://www.w3.org/2000/svg" width="1em"
-                                            height="1em" fill="currentColor" viewBox="0 0 16 16">
-                                            <path
-                                                d="M8 4.754a3.246 3.246 0 1 0 0 6.492 3.246 3.246 0 0 0 0-6.492M5.754 8a2.246 2.246 0 1 1 4.492 0 2.246 2.246 0 0 1-4.492 0">
-                                            </path>
-                                            <path
-                                                d="M9.796 1.343c-.527-1.79-3.065-1.79-3.592 0l-.094.319a.873.873 0 0 1-1.255.52l-.292-.16c-1.64-.892-3.433.902-2.54 2.541l.159.292a.873.873 0 0 1-.52 1.255l-.319.094c-1.79.527-1.79 3.065 0 3.592l.319.094a.873.873 0 0 1 .52 1.255l-.16.292c-.892 1.64.901 3.434 2.541 2.54l.292-.159a.873.873 0 0 1 1.255.52l.094.319c.527 1.79 3.065 1.79 3.592 0l.094-.319a.873.873 0 0 1 1.255-.52l.292.16c1.64.893 3.434-.902 2.54-2.541l-.159-.292a.873.873 0 0 1 .52-1.255l.319-.094c1.79-.527 1.79-3.065 0-3.592l-.319-.094a.873.873 0 0 1-.52-1.255l.16-.292c.893-1.64-.902-3.433-2.541-2.54l-.292.159a.873.873 0 0 1-1.255-.52zm-2.633.283c.246-.835 1.428-.835 1.674 0l.094.319a1.873 1.873 0 0 0 2.693 1.115l.291-.16c.764-.415 1.6.42 1.184 1.185l-.159.292a1.873 1.873 0 0 0 1.116 2.692l.318.094c.835.246.835 1.428 0 1.674l-.319.094a1.873 1.873 0 0 0-1.115 2.693l.16.291c.415.764-.42 1.6-1.185 1.184l-.291-.159a1.873 1.873 0 0 0-2.693 1.116l-.094.318c-.246.835-1.428.835-1.674 0l-.094-.319a1.873 1.873 0 0 0-2.692-1.115l-.292.16c-.764.415-1.6-.42-1.184-1.185l.159-.291A1.873 1.873 0 0 0 1.945 8.93l-.319-.094c-.835-.246-.835-1.428 0-1.674l.319-.094A1.873 1.873 0 0 0 3.06 4.377l-.16-.292c-.415-.764.42-1.6 1.185-1.184l.292.159a1.873 1.873 0 0 0 2.692-1.115z">
-                                            </path>
-                                        </svg>&nbsp;Settings</a><a class="dropdown-item" href="#"><svg
-                                            class="bi bi-list-nested me-2" xmlns="http://www.w3.org/2000/svg"
-                                            width="1em" height="1em" fill="currentColor" viewBox="0 0 16 16">
-                                            <path fill-rule="evenodd"
-                                                d="M4.5 11.5A.5.5 0 0 1 5 11h10a.5.5 0 0 1 0 1H5a.5.5 0 0 1-.5-.5m-2-4A.5.5 0 0 1 3 7h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m-2-4A.5.5 0 0 1 1 3h10a.5.5 0 0 1 0 1H1a.5.5 0 0 1-.5-.5">
-                                            </path>
-                                        </svg>&nbsp;Activity log</a>
-                                    <div class="dropdown-divider"></div><a class="dropdown-item link-danger"
-                                        href="logout.php"><svg class="bi bi-box-arrow-right me-2"
-                                            xmlns="http://www.w3.org/2000/svg" width="1em" height="1em"
-                                            fill="currentColor" viewBox="0 0 16 16">
-                                            <path fill-rule="evenodd"
-                                                d="M10 12.5a.5.5 0 0 1-.5.5h-8a.5.5 0 0 1-.5-.5v-9a.5.5 0 0 1 .5-.5h8a.5.5 0 0 1 .5.5v2a.5.5 0 0 0 1 0v-2A1.5 1.5 0 0 0 9.5 2h-8A1.5 1.5 0 0 0 0 3.5v9A1.5 1.5 0 0 0 1.5 14h8a1.5 1.5 0 0 0 1.5-1.5v-2a.5.5 0 0 0-1 0z">
-                                            </path>
-                                            <path fill-rule="evenodd"
-                                                d="M15.854 8.354a.5.5 0 0 0 0-.708l-3-3a.5.5 0 0 0-.708.708L14.293 7.5H5.5a.5.5 0 0 0 0 1h8.793l-2.147 2.146a.5.5 0 0 0 .708.708z">
-                                            </path>
-                                        </svg>&nbsp;Logout</a>
-                                </div>
-                            </div>
-                        </div>
-                        <div>
-                            <ol class="breadcrumb">
-                                <li class="breadcrumb-item"><a href="#"><span>Home</span></a></li>
-                                <li class="breadcrumb-item"><a href="#"><span>Examples</span></a></li>
-                                <li class="breadcrumb-item active"><span>Documents</span></li>
-                            </ol>
-                            <h1 class="h2">Welcome <?php echo $_SESSION['user_email']; ?></h1>
-                        </div>
-                    </div>
+                    <?php include '_include/nav_top.php'; ?>
                     <div>
                         <div class="row">
                             <div class="col-md-6 col-xl-3 mb-4">
