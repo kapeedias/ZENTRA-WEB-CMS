b940b94a881a015577fe8ab7629b0e3d2d748182
Sai Deepak <sai@livewd.ca>
2026-05-05 23:17:06 -0700

    plugins

--- _include/body_end_plugins.php
+++ _include/body_end_plugins.php
@@ -3,7 +3,7 @@
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
 <script src="https://cdn.datatables.net/1.13.8/js/jquery.dataTables.min.js"></script>
 <script src="https://cdn.datatables.net/1.13.8/js/dataTables.bootstrap5.min.js"></script>
-<script src="assets/js/script.min.js?h=76fb943b07981bddcd684084e3798cff"></script>
+<script src="/assets/js/script.min.js?h=76fb943b07981bddcd684084e3798cff"></script>
 <script>
 const table = $('#AppConfigData').DataTable({
     pageLength: 10,
--- _include/head.php
+++ _include/head.php
@@ -1,8 +1,8 @@
-<link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css?h=283928673d7441cd64f1af3db9200eab">
+<link rel="stylesheet" href="/assets/bootstrap/css/bootstrap.min.css?h=283928673d7441cd64f1af3db9200eab">
 <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Geist:400,700&amp;display=swap">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/ionicons/2.0.1/css/ionicons.min.css">
 <link rel="stylesheet"
     href="https://cdnjs.cloudflare.com/ajax/libs/material-design-icons/3.0.1/iconfont/material-icons.min.css">
-<link rel="stylesheet" href="assets/css/styles.min.css?h=8185e874b995279ecb2a9d7bb55b09b0">
+<link rel="stylesheet" href="/assets/css/styles.min.css?h=8185e874b995279ecb2a9d7bb55b09b0">
 <link rel="stylesheet" href="https://cdn.datatables.net/1.13.8/css/dataTables.bootstrap5.min.css">
