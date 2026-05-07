a5c2cc53bfeb8a89b738e0791430869698e4ee1d
Sai Deepak <sai@livewd.ca>
2026-05-07 15:10:01 -0700

    dd

--- _include/body_end_plugins.php
+++ _include/body_end_plugins.php
@@ -4,29 +4,6 @@
 <script src="https://cdn.datatables.net/1.13.8/js/jquery.dataTables.min.js"></script>
 <script src="https://cdn.datatables.net/1.13.8/js/dataTables.bootstrap5.min.js"></script>
 <script src="/assets/js/script.min.js?h=76fb943b07981bddcd684084e3798cff"></script>
-<script>
-const table = $('#AppConfigData').DataTable({
-    pageLength: 10,
-    lengthMenu: [5, 10, 25, 50, 75, 100],
-    ordering: true,
-    searching: true,
-    autoWidth: false, // IMPORTANT
-    scrollX: false, // IMPORTANT (prevents forced horizontal scroll)
-    dom: '<"row align-items-center mb-2"<"col-md-6"l>>' +
-        'rt' +
-        '<"row align-items-center mt-2"<"col-md-6"i><"col-md-6 d-flex justify-content-end"p>>'
-});
-//search
-let timer;
-$('input[name="searchAppConfig"]').on('input', function() {
-    clearTimeout(timer);
-    const value = this.value;
-
-    timer = setTimeout(() => {
-        table.search(value).draw();
-    }, 150);
-});
-</script>
 
 <script>
 document.addEventListener('DOMContentLoaded', function() {
@@ -68,6 +45,7 @@ document.addEventListener('DOMContentLoaded', function() {
 
 });
 </script>
+
 
 
 <script>
@@ -115,5 +93,5 @@ function setAllDayEvent(isAllDay) {
     }
 
     validateEventTimes();
-}
-</script>
+} <
+/>
