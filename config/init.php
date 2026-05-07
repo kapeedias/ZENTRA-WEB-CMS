57e94428781ea89587ce38ad2252a92ad2057516
Sai Deepak <sai@livewd.ca>
2026-05-07 16:44:28 -0700

    test init

--- config/init.php
+++ config/init.php
@@ -2,12 +2,7 @@
 require_once __DIR__ . '/db.php';
 
 // GoDaddy → manual credentials
-$pdo = Database::getInstance(
-    'localhost',
-    'your_db_name',
-    'your_db_user',
-    'your_db_pass'
-);
+$pdo = Database::getInstance();
 
 // Azure → environment variables
 // $pdo = Database::getInstance();
