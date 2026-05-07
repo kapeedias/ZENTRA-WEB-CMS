b6067716d86506a7095511504ae4aca45155d1bb
Sai Deepak <sai@livewd.ca>
2026-05-06 23:55:19 -0700

    sta

--- events-edit.php
+++ events-edit.php
@@ -118,9 +118,9 @@
                         <div class="row">
                             <div class="col-xl-8 mb-4">
                                 <div class="card mb-4">
-                                    <div class="card-header d-flex justify-content-between align-items-center">
+                                    <div class="card-header d-flex justify-content-end align-items-center">
                                         <h5 class="fw-bold mb-0"></h5>
-                                        <span class="badge d-inline-flex gap-1 <?php echo $badge['class']; ?>">
+                                        Status: <span class="badge d-inline-flex gap-1 <?php echo $badge['class']; ?>">
                                             <i class="fa <?php echo $badge['icon']; ?> me-1"></i>
                                             <?php echo $badge['label']; ?>
                                         </span>
