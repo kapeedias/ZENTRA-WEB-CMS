c19ca1f558c55e1c97d7ed854abbd46dd36e9c03
Sai Deepak <sai@livewd.ca>
2026-05-08 00:15:32 -0700

    test ah

--- events-edit.php
+++ events-edit.php
@@ -613,9 +613,6 @@
             </div>
         </div>
     </div>
-
-    <?php include '_include/body_end_plugins.php'; ?>
-    <!-- Include Quill -->
     <script src="https://cdn.jsdelivr.net/npm/quill@2.0.3/dist/quill.js"></script>
 
     <script>
@@ -665,6 +662,9 @@
         return true;
     }
     </script>
+    <?php include '_include/body_end_plugins.php'; ?>
+    <!-- Include Quill -->
+
 </body>
 
 
