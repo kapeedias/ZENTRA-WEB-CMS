d9dc34922805def35013cf2f0b33e2a5e7ca0d06
Sai Deepak <sai@livewd.ca>
2026-05-08 01:16:43 -0700

    modal center

--- events-edit.php
+++ events-edit.php
@@ -614,7 +614,7 @@
         </div>
     </div>
     <div class="modal fade" id="zentraMediaModal" tabindex="-1" data-bs-backdrop="static" data-bs-keyboard="false">
-        <div class="modal-dialog modal-lg">
+        <div class="modal-dialog modal-lg modal-dialog-centered">
             <div class="modal-content">
 
                 <div class="modal-header">
