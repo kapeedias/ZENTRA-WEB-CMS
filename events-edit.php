e84a3590e06128a342fd54c53467bd5a3e969c8b
Sai Deepak <sai@livewd.ca>
2026-05-21 17:18:24 -0700

    re

--- events-edit.php
+++ events-edit.php
@@ -290,7 +290,7 @@
                                                 Poster</h5>
                                         </div>
                                         <div class="card-body pt-2"><img class="img-fluid rounded w-100 h-100"
-                                                width="590" height="393" src="assets/img/1200x600.jpg">
+                                                width="590" height="393" src="/assets/img/1200x600.jpg">
                                             <hr>
                                             <div class="small text-muted mb-2"><span>Social
                                                     Links</span></div>
