3a81193b5d07202898278d6633816adbc55a9387
Sai Deepak <sai@livewd.ca>
2026-05-29 00:34:08 -0700

    event poster

--- ajax/update_event_poster.php
+++ ajax/update_event_poster.php
@@ -11,10 +11,11 @@ $input = json_decode(file_get_contents("php://input"), true);
 
 $eventId   = (int) $input['event_id'];
 $libraryId = (int) $input['library_id'];
-$tenantId  = (int) $_SESSION['tenant_id'];
+$tenantId  = (int) $_SESSION['tenant_id'] ?? 0;
 
 $pdo    = Database::getInstance();
-$events = new EventsModule($pdo, $tenantId);
+$logger = new ActivityLogger($pdo, $tenantId);
+$events = new EventsModule($pdo, $tenantId, $logger);
 
 $success = $events->updateEventPoster($eventId, $libraryId, $tenantId);
 
